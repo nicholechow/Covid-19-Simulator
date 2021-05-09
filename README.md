7ef148900c451b1e2a19b4b07c9d2218984a4823
NicoleChow08 <nicolechow08@gmail.com>
2021-05-09 10:36:05 +0800

    simulation basic done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Covid-19-Pool-Testing
+In order to run the simulation smoothly, mouse should be stationary
--- simulation.m
+++ simulation.m
@@ -1,28 +1,32 @@
-close all
 clear all
-human=100;
-sample=rand(human,2);
-sample=[sample (randi([-1 0],100,1)*2+1).*rand(human,1) (randi([-1 0],100,1)*2+1).*rand(human,1) randi([-1 0],human,1) 0*eye(human,1)];
-dt=0.01;
-i=1;
-p=0.05;
-while ~any(sample(:,6))
-    for j=1:human
-        if rand<=0.05
-            sample(j,6)=1;
-        end
-    end
-end
-while true
-    if i == 1
+close all
+human=200;
+sample.x=rand(human,1);
+sample.y=rand(human,1);
+sample.u=(randi([-1 0],human,1)*2+1).*rand(human,1);
+sample.v=(randi([-1 0],human,1)*2+1).*rand(human,1);
+sample.sick=0*eye(human,1);
+sample.sick_time=0*eye(human,1);
+sample.
+dt=0.02;
+i=0;
+death_rate=0.02;
+temp=randi([1 human],1);
+sample.sick(temp)=1; %Intorducing a random sick
+
+while ~isempty(find(sample.sick==1))
+    if i == 0
         for k = 1 : human
             hold on;
-            switch sample(k,6)
-                case 0
-                plot(sample(k,1), sample(k,2), '.k','MarkerSize',20);
-                case 1
-                    plot(sample(k,1), sample(k,2), '.r','MarkerSize',20);
-                case 2
+            switch sample.sick(k)
+                case 0 %Healthy
+                    plot(sample.x(k), sample.y(k), '.b','MarkerSize',20);
+                case 1 %Sick
+                    plot(sample.x(k), sample.y(k), '.r','MarkerSize',20);
+                case 2 %Recovered
+                    plot(sample.x(k), sample.y(k), '.g','MarkerSize',20);
+                case 3 %Dead
+                    plot(sample.x(k), sample.y(k), '.k','MarkerSize',20);
             end
             axis([-0.05 1.05 -0.05 1.05])
             hold off;
@@ -30,31 +34,62 @@ while true
     else
         h = findobj(gca,'Type','line');
         for k=1:human
-            x=sample(k,1)+sample(k,3)*dt;
-            y=sample(k,2)+sample(k,4)*dt;
+            if sample.sick(k)~=3
+                x=sample.x(k)+sample.u(k)*dt;
+                y=sample.y(k)+sample.v(k)*dt;
+            else
+                x=sample.x(k);
+                y=sample.y(k);
+            end
             if x > 1-dt
-                sample(k,1) = sample(k,1)*(1-dt);
-                sample(k,3) = -sample(k,3);
+                sample.x(k) = sample.x(k)*(1-dt);
+                sample.u(k) = -sample.u(k);
             end
             if y > 1-dt
-                sample(k,2) = sample(k,2)*(1-dt);
-                sample(k,4) = -sample(k,4);
+                sample.y(k) = sample.y(k)*(1-dt);
+                sample.v(k) = -sample.v(k);
             end
             if x < -dt
-                sample(k,1) = sample(k,1)*(1-dt);
-                sample(k,3) = -sample(k,3);
+                sample.x(k) = sample.x(k)*(1-dt);
+                sample.u(k) = -sample.u(k);
             end
             if y < -dt
-                sample(k,2) = sample(k,2)*(1-dt);
-                sample(k,4) = -sample(k,4);
+                sample.y(k) = sample.y(k)*(1-dt);
+                sample.v(k) = -sample.v(k);
             end
-            sample(k,1)=x;
-            sample(k,2)=y;
-            set(h(k),'XData',x)
-            set(h(k),'YData',y)
+            sample.x(k)=x;
+            sample.y(k)=y;
+            if sample.sick(k)==1
+                for jj=1:human
+                    if k~=jj &&  norm([sample.x(jj) sample.y(jj)]-[sample.x(k) sample.y(k)])<=0.01 && sample.sick(jj)==0
+                        sample.sick(jj)=1;
+                        sample.sick_time(jj)= i*dt;
+                    end
+                end
+            end
+            
+            for ii=1:human
+                if dt*i-sample.sick_time(ii)==0.5 && sample.sick(ii)==1 && dt*i>=1
+                    if rand<=death_rate
+                        sample.sick(ii)=3;
+                    else
+                        sample.sick(ii)=2;
+                    end
+                end 
+            end
+            
+            switch sample.sick(k)
+                case 0 %Healthy
+                    set(h(k),'XData',x,'YData',y,'Color','b')
+                case 1 %Sick
+                    set(h(k),'XData',x,'YData',y,'Color','r','Marker')
+                case 2 %Recovered
+                    set(h(k),'XData',x,'YData',y,'Color','g')
+                case 3 %Dead
+                    set(h(k),'XData',x,'YData',y,'Color','k')
+            end
         end
         drawnow
-        pause(0.00000000001);
     end
     i=i+1;
 end
